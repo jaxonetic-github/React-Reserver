6d888977505aef83420aa0f41a4dddd411308aa9
Alonzo Jackson <jaxonetic@gmail.com>
2021-12-02 06:50:54 -0700

    react router v6 for gh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MainComponent from "./home/MainComponent.js";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import GeneralInfo from './home/GeneralInfo';
+//import GeneralInfo from './home/GeneralInfo';
 import Reservations from './checkout/Reservations';
 import Checkout from './checkout/Checkout';
 import Profile from './Profile';
@@ -20,13 +20,14 @@ import SignUp from './signinup/Signup';
 /**
  *  the Main Component containing the app routes          
  *      <Route path="*" element={<GeneralInfo />} />
+ *     <Route exact path={'/React-Reserver}  element={<GeneralInfo />} />
+
  */
 function App() {
 console.log(process.env.REACT_APP_WEBPATH,',',process.env.PUBLIC_URL)
   return (<Router basename='/React-Reserver'>
      <Routes basename='/React-Reserver' >
-          <Route exact path={'/React-Reserver/'} element={<MainComponent />} >
-    <Route exact path={/React-Reserver}  element={<GeneralInfo />} />
+          <Route exact path={'/'} element={<MainComponent />} >
              <Route path="/profile" element={<Profile/>} />
              <Route exact path={`${process.env.PUBLIC_URL}/signin`} element={<SignIn />} />
              <Route exact path="/signup" element={<SignUp />} />
