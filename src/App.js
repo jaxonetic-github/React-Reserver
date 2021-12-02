dec9ec2576cd90b310f1dceac22d5693efed70f9
Alonzo Jackson <jaxonetic@gmail.com>
2021-12-01 19:44:16 -0700

    homepage

--- src/App.js
+++ src/App.js
@@ -23,9 +23,9 @@ import SignUp from './signinup/Signup';
  */
 function App() {
 console.log(process.env.REACT_APP_WEBPATH,',',process.env.PUBLIC_URL)
-  return (<BrowserRouter basename={`${process.env.PUBLIC_URL}`} >
-     <Routes >
-          <Route exact path="" element={<MainComponent />} >
+  return (<BrowserRouter >
+     <Routes basename={`${process.env.PUBLIC_URL}`} >
+          <Route exact path="/" element={<MainComponent />} >
     <Route index element={<GeneralInfo />} />
              <Route path="profile" element={<Profile/>} />
              <Route path="signin" element={<SignIn />} />
