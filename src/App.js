96f8807f484303dd87b6256022536e90c32b287e
Alonzo Jackson <jaxonetic@gmail.com>
2021-12-03 09:28:07 -0700

    react router v6

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import SignUp from './signinup/Signup';
 function App() {
 console.log(process.env.REACT_APP_WEBPATH,',',process.env.PUBLIC_URL)
   return (<Router basename={'/React-Reserver'}>
-          <Route exact path={'/React-Reserver/'} element={<MainComponent />} />
+          <Route exact path={'/'} element={<MainComponent />} />
              <Route path={'/React-Reserver/'} element={<GeneralInfo />} />
              <Route path={"/profile"} element={<Profile/>} />
              <Route  exact path={`/React-Reserver/signin`} element={<SignIn />} />
