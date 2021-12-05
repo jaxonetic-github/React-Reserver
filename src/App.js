bc79661f00c300b468c90f50cc952015f2dfd341
Alonzo Jackson <jaxonetic@gmail.com>
2021-12-05 00:02:34 -0700

    hashrouter v6

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import SignUp from './signinup/Signup';
  */
 function App() {
 console.log(process.env.REACT_APP_WEBPATH,',',process.env.PUBLIC_URL)
-  return (<Router basename=''>
+  return (<Router basename='/React-Reserver'>
     <Routes >
           <Route  exact path={'/React-Reserver'} element={<MainComponent />} >
              <Route  index   element={<GeneralInfo />} />
